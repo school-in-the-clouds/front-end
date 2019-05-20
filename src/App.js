9a0b12ae7d67d8035ce2107a482cddba27124808
Moses Samuel <mosamuel98@gmail.com>
2019-05-20 11:50:40 -0700

    render Nav and SignUpForm components inside App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import logo from '../assets/img/school-in-the-cloud-logo.png'
+
+import Prompt from './components/ui/Prompt.jsx'
+import SignUpForm from './components/ui/SignUpForm.jsx'
+import Nav from './components/ui/Nav.jsx/index.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Nav logo={logo} />
       </header>
+      <Prompt message={"Hi, is this your first time here?" +  "Y/N"}  />
+      <SignUpForm />
+      
     </div>
   );
 }
