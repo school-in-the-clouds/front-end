42bdc6cdfacba8e1d5bf8488b78767fac95281d8
Moses Samuel <mosamuel98@gmail.com>
2019-05-20 12:32:18 -0700

    move imgs to src folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import './App.css';
-import logo from '../assets/img/school-in-the-cloud-logo.png'
+import logo from './assets/img/school-in-the-cloud-logo.png'
 
 import Prompt from './components/ui/Prompt.jsx'
 import SignUpForm from './components/ui/SignUpForm.jsx'
-import Nav from './components/ui/Nav.jsx/index.js'
+import Nav from './components/ui/Nav.jsx'
 
 function App() {
   return (
