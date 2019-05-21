8228194fbc9e4e02d7ccc0fd0e73d7ddae93cad1
Moses Samuel <mosamuel98@gmail.com>
2019-05-21 15:31:46 -0700

    use NavLinks from react-router for Nav jsx component

--- src/components/ui/Nav.jsx
+++ src/components/ui/Nav.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 
 export default function Nav(props) {
     return (
         <nav>
-            <img src={props.logo || "#"} alt="Logo" />
-            <a>SignUp/Login</a>
-            <a>About</a>
-            <a>Contact</a>
+            <NavLink to="/">
+                <img src={props.logo || "#"} alt="Logo" />
+            </NavLink>
+            <NavLink to="/login">SignUp/Login</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
         </nav>
     )
 }
