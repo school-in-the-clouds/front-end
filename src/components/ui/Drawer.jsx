37ebac116455d2cbf4e435a5d1db45451e78ed5c
Moses Samuel <mosamuel98@gmail.com>
2019-05-21 13:16:20 -0700

    add Drawer jsx component

--- src/components/ui/Drawer.jsx
+++ src/components/ui/Drawer.jsx
@@ -1,7 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-export default function Drawer() {
+export default function Dashboard(props) {
+    let [isOpen, toggleIsOpen] = useState(false)
+
+    const handleClick = _ => {
+        toggleIsOpen(!isOpen)
+    }
+    
     return (
-        <div className="drawer">Drawer</div>
+        (isOpen) 
+            ? <div className="drawer--open" >
+                <HamburgerIcon onClick={handleClick} />
+                { ...props.children }
+              </ div>
+              
+            : <div className="drawer--close">
+                <HamburgerIcon onClick={handleClick} />
+              </div>
+
     )
+}
+
+function HamburgerIcon(props) {
+    return <img src="https://img.icons8.com/material/24/000000/menu.png"/>
 }
