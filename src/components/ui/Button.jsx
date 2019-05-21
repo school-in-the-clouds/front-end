f5390ac8f25ec13278e915e0575e4c51dbe27f5c
Moses Samuel <mosamuel98@gmail.com>
2019-05-21 09:56:10 -0700

    add Button jsx component

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useStyletron } from "styletron-react"
+
+export default function Button(props) {
+  const [css] = useStyletron()
+  
+  return (
+    <>
+    { (props.quantity)
+        ? props.quantity.map((x => (
+                <button className={css(
+                  (props.styles)
+                    ? { ...props.styles }
+                    : { color: props.color || "blue" }
+                  )}>
+                    {props.text || ""}
+                </button>
+            )
+        ))
+        :  <button className={css(
+            (props.styles)
+              ? { ...props.styles }
+              : { color: props.color || "blue" }
+            )}>{props.text || "Click me"}</button>
+    }
+    </> 
+  )
+}
+
+
