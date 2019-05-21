55bd838f0713c3379bae43409d460b01abeaec00
Moses Samuel <mosamuel98@gmail.com>
2019-05-21 09:36:27 -0700

    import Styletron Provider

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+
+// 1. Create a client engine instance
+const engine = new Styletron();
+
+ReactDOM.render(
+<StyletronProvider value={engine}>
+    <App />
+</StyletronProvider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
