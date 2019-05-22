6e4affd1a99bd809f90104fd89caef92c13de343
Moses Samuel <mosamuel98@gmail.com>
2019-05-22 13:27:37 -0700

    add actions/ action creators for tasks

---
+++ src/actions/task.js
@@ -0,0 +1,72 @@
+// admins can perform CRUD operations on tasks
+// TODO: fetch data w/ axios and use dispatch
+
+
+// CREATE
+
+export const ADD_TASK_INIT = "ADD_TASK_INIT"
+export const ADD_TASK_SUCCESS = "ADD_TASK_SUCCESS"
+export const ADD_TASK_FAILURE = "ADD_TASK_FAILURE"
+
+export const addTask = (newTask) => (dispatch) =>
+    ({
+        type: ADD_TASK_INIT,
+        payload: newTask
+    })
+
+
+// READ 
+
+export const GET_TASK_INIT = "GET_TASK_INIT"
+export const GET_TASK_SUCCESS = "GET_TASK_SUCCESS"
+export const GET_TASK_FAILURE = "GET_TASK_FAILURE"
+
+export const getTask = (task) => (dispatch) => 
+    ({
+        type: GET_TASK_INIT,
+        payload: task
+    })
+
+export const GET_ALL_TASKS_INIT = "GET_ALL_TASKS_INIT"
+export const GET_ALL_TASKS_SUCCESS = "GET_ALL_TASKS_SUCCESS"
+export const GET_ALL_TASKS_FAILURE = "GET_ALL_TASKS_FAILURE"
+
+export const getAllTasks = () => (dispatch) => 
+    ({ type: GET_ALL_TASKS_INIT })
+
+
+// UPDATE
+
+export const EDIT_TASK_INIT = "EDIT_TASK_INIT"
+export const EDIT_TASK_SUCCESS = "EDIT_TASK_SUCCESS"
+export const EDIT_TASK_FAILURE = "EDIT_TASK_FAILURE"
+
+export const editTask = (task) => (dispatch) =>
+    ({
+        type: EDIT_TASK_INIT,
+        payload: task
+    })
+
+
+export const MARK_COMPLETE_INIT = "MARK_COMPLETE"
+export const MARK_COMPLETE_SUCCESS = "MARK_COMPLETE"
+export const MARK_COMPLETE_FAILURE = "MARK_COMPLETE"
+
+export const markComplete = (task) => (dispatch) => 
+    ({
+        type: MARK_COMPLETE_INIT,
+        payload: task
+    })
+
+
+// DELETE
+
+export const DELETE_TASK_INIT = "DELETE_TASK_INIT"
+export const DELETE_TASK_SUCCESS = "DELETE_TASK_SUCCESS"
+export const DELETE_TASK_FAILURE = "DELETE_TASK_FAILURE"
+
+export const deleteTask = (task) => (disptach) => 
+    ({
+        type: DELETE_TASK_INIT,
+        payload: task
+    })
