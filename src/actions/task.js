779e7bcb4243bd6bbcae970d9da9fc8cc56aff15
Moses Samuel <mosamuel98@gmail.com>
2019-05-22 15:50:09 -0700

    properly dispatch thunks + add optional currying

--- src/actions/task.js
+++ src/actions/task.js
@@ -4,6 +4,7 @@
 
 
 import axios from 'axios'
+import { curry } from 'ramda'
 
 // TODO: use ramda to curry action creators
 // TODO: use useContext to pull auth token off local storage (instead of resorting to optional currying)
@@ -16,11 +17,10 @@ export const ADD_TASK_INIT = "ADD_TASK_INIT"
 export const ADD_TASK_SUCCESS = "ADD_TASK_SUCCESS"
 export const ADD_TASK_FAILURE = "ADD_TASK_FAILURE"
 
-export const addTask = (newTask) => (dispatch) =>
-    ({
-        type: ADD_TASK_INIT,
-        payload: newTask
-    })
+export const addTask = curry((authToken, newTask) => (dispatch) => {
+    dispatch({ type: ADD_TASK_INIT })
+})
+    
 
 
 
@@ -31,7 +31,7 @@ export const GET_TASK_INIT = "GET_TASK_INIT"
 export const GET_TASK_SUCCESS = "GET_TASK_SUCCESS"
 export const GET_TASK_FAILURE = "GET_TASK_FAILURE"
 
-export const getTaskById = (authToken, taskId) => (dispatch) => {
+export const getTaskById = curry((authToken, taskId) => (dispatch) => {
     dispatch({ type: GET_TASK_INIT })
     axios.get(
         `https://school-itc.herokuapp.com/api/tasks/${taskId}`, 
@@ -49,9 +49,9 @@ export const getTaskById = (authToken, taskId) => (dispatch) => {
             payload: err
         })
     })
-}
+})
 
-export const getTaskByVolunteer = (authToken, volunteerId) => (dispatch) => {
+export const getTaskByVolunteer = curry((authToken, volunteerId) => (dispatch) => {
     dispatch({ type: GET_TASK_INIT })
 
     axios.get(
@@ -70,7 +70,7 @@ export const getTaskByVolunteer = (authToken, volunteerId) => (dispatch) => {
             payload: err
         })
     })
-}
+})
 
 export const GET_ALL_TASKS_INIT = "GET_ALL_TASKS_INIT"
 export const GET_ALL_TASKS_SUCCESS = "GET_ALL_TASKS_SUCCESS"
@@ -106,22 +106,20 @@ export const EDIT_TASK_INIT = "EDIT_TASK_INIT"
 export const EDIT_TASK_SUCCESS = "EDIT_TASK_SUCCESS"
 export const EDIT_TASK_FAILURE = "EDIT_TASK_FAILURE"
 
-export const editTask = (task) => (dispatch) =>
-    ({
-        type: EDIT_TASK_INIT,
-        payload: task
-    })
+export const editTask = curry((authToken, task) => (dispatch) => {
+    dispatch ({ type: EDIT_TASK_INIT })
+    dispatch ({ type: EDIT_TASK_FAILURE })
+})
 
 
 export const MARK_COMPLETE_INIT = "MARK_COMPLETE"
 export const MARK_COMPLETE_SUCCESS = "MARK_COMPLETE"
 export const MARK_COMPLETE_FAILURE = "MARK_COMPLETE"
 
-export const markComplete = (task) => (dispatch) => 
-    ({
-        type: MARK_COMPLETE_INIT,
-        payload: task
-    })
+export const markComplete = curry((authToken, task) => (dispatch) => {
+    dispatch({ type: MARK_COMPLETE_INIT })
+    dispatch({ type: MARK_COMPLETE_FAILURE })
+})
 
 
 
@@ -132,11 +130,10 @@ export const DELETE_TASK_INIT = "DELETE_TASK_INIT"
 export const DELETE_TASK_SUCCESS = "DELETE_TASK_SUCCESS"
 export const DELETE_TASK_FAILURE = "DELETE_TASK_FAILURE"
 
-export const deleteTask = (task) => (disptach) => 
-    ({
-        type: DELETE_TASK_INIT,
-        payload: task
-    })
+export const deleteTask = curry((authToken, task) => (dispatch) => {
+    dispatch({ type: DELETE_TASK_INIT })
+    dispatch({ type: DELETE_TASK_FAILURE })
+})
 
 
 
