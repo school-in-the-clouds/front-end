e702c12e30c26e8f03c72649959043f99dd6f0ae
Moses Samuel <mosamuel98@gmail.com>
2019-05-22 13:48:57 -0700

    refactor: use axios for data fetching in auth AC

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,3 +1,6 @@
+import axios from 'axios'
+
+
 // ---- LOGIN ----
 
 export const LOG_IN_INIT = "LOG_IN_INIT"
@@ -6,30 +9,24 @@ export const LOG_IN_FAILURE = "LOG_IN_FAILURE"
 
 // loginUser: Obj -> void
 export const loginUser = (userInfo) => (dispatch) => {
-     // TODO: use axios
-     dispatch({type: SIGN_UP_INIT})
-     fetch("https://school-itc.herokuapp.com/api/accounts/login",{
-         method: 'POST',        
-         headers: { 'Content-Type': 'application/json' },
-         body: JSON.stringify(userInfo), 
-     })
-     .then(res => res.json())
-     .then(res => {
+    dispatch({ type: SIGN_UP_INIT })
+    axios.post("https://school-itc.herokuapp.com/api/accounts/login", userInfo)
+    .then(res => {
          dispatch({
-             type: SIGN_UP_SUCCESS,
-             payload: res
-         })
-     })
+            type: SIGN_UP_SUCCESS,
+            payload: res
+        })
+    })
      .catch(err => { 
-         dispatch({
+        dispatch({
             type: SIGN_UP_FAILURE, 
             payload: err 
-         })
-     })
+        })
+    })
 }
 
-//  ---- SIGN UP ----
 
+//  ---- SIGN UP ----
 
 export const SIGN_UP_INIT = "SIGN_UP_INIT"
 export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS"
@@ -38,14 +35,8 @@ export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE"
 
 // registerUser:  Obj -> void
 export const registerUser = (userInfo) => (dispatch) => {
-    // TODO: use axios
-    dispatch({type: SIGN_UP_INIT})
-    fetch("https://school-itc.herokuapp.com/api/accounts/register",{
-        method: 'POST',        
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(userInfo), 
-    })
-    .then(res => res.json())
+    dispatch({ type: SIGN_UP_INIT })
+    axios.post("https://school-itc.herokuapp.com/api/accounts/register", userInfo)
     .then(res => {
         dispatch({
             type: SIGN_UP_SUCCESS,
