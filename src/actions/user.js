5271c0b7d2e7a98a0d89ce37959dbd4482e0b3b5
Moses Samuel <mosamuel98@gmail.com>
2019-05-22 13:27:13 -0700

    add actions/action creators for users

---
+++ src/actions/user.js
@@ -0,0 +1,21 @@
+// users can edit their username or password
+
+export const EDIT_PASSWORD_INIT = "EDIT_PASSWORD_INIT"
+export const EDIT_PASSWORD_SUCCESS = "EDIT_PASSWORD_SUCCESS"
+export const EDIT_PASSWORD_FAILURE = "EDIT_PASSWORD_FAILURE"
+
+export const editPassword = (newPassword) => (dispatch) => 
+    ({
+        type: "EDIT_PASSWORD_INIT",
+        payload: newPassword
+    })
+
+export const EDIT_USERNAME_INIT = "EDIT_USERNAME_INIT"
+export const EDIT_USERNAME_SUCCESS = "EDIT_USERNAME_SUCCESS"
+export const EDIT_USERNAME_FAILURE = "EDIT_USERNAME_FAILURE"
+
+export const editUsername = (newUsername) => (dispatch) =>
+    ({
+        type: "EDIT_PASSWORD_INIT",
+        payload: newUsername
+    })
