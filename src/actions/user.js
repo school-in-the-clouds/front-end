05aee43e96149054f45713b372081b06b2312096
Moses Samuel <mosamuel98@gmail.com>
2019-05-22 14:10:54 -0700

    properly use dispatch in "user" thunk

--- src/actions/user.js
+++ src/actions/user.js
@@ -10,18 +10,21 @@ export const EDIT_PASSWORD_INIT = "EDIT_PASSWORD_INIT"
 export const EDIT_PASSWORD_SUCCESS = "EDIT_PASSWORD_SUCCESS"
 export const EDIT_PASSWORD_FAILURE = "EDIT_PASSWORD_FAILURE"
 
-export const editPassword = (newPassword) => (dispatch) => 
-    ({
-        type: "EDIT_PASSWORD_INIT",
-        payload: newPassword
-    })
+// editPassword: String -> (x -> void) -> void
+export const editPassword = (newPassword) => (dispatch) => {
+    dispatch({ type: "EDIT_PASSWORD_INIT" })
+    // axios.post()
+    // TODO: get endpoint for handling password updates
+}
+
 
 export const EDIT_USERNAME_INIT = "EDIT_USERNAME_INIT"
 export const EDIT_USERNAME_SUCCESS = "EDIT_USERNAME_SUCCESS"
 export const EDIT_USERNAME_FAILURE = "EDIT_USERNAME_FAILURE"
 
-export const editUsername = (newUsername) => (dispatch) =>
-    ({
-        type: "EDIT_PASSWORD_INIT",
-        payload: newUsername
-    })
+// editUsername: String -> (x -> void) -> void
+export const editUsername = (newUsername) => (dispatch) => {
+    dispatch({ type: "EDIT_PASSWORD_INIT" })
+    // axios.post()
+    // TODO: get endpoint for handling username updates
+}
