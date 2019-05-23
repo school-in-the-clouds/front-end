9e276c50bed6954c297044c0d3f98dd4d14270f3
Moses Samuel <mosamuel98@gmail.com>
2019-05-23 10:23:12 -0700

    fill in logic for auth reducer

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -7,24 +7,58 @@ import {
 const initialState = {
     fetching: false,
     loggedIn: false,
-    errors: [],
-    authToken: localStorage.getItem('school-in-the-cloud-auth-key'),
-
+    error: null,
     name: '',
-    password: '',
     role: null,
+    id: null,
+    authToken: localStorage.getItem('school-in-the-cloud-auth-key'),
 }
 
 
 export default function userReducer(state=initialState, action) {
     switch(action.type) {
         case LOG_IN_INIT:
+            return {
+                ...state, 
+                fetching: true
+            }
         case LOG_IN_SUCCESS:
+            const { token, role, name, id } = action.payload
+
+            return {
+                ...state,
+                fetching: false,
+                error: null,
+                loggedIn: true,
+                id,
+                name,
+                role,
+                authToken: token,
+            }
         case LOG_IN_FAILURE:
+            return {
+                ...state,
+                fetching: false,
+                error: action.payload,
+            }
 
         case SIGN_UP_INIT:
-        case SIGN_UP_SUCCESS:
+            return {
+                ...state,
+                fetching: true
+            }
+        case SIGN_UP_SUCCESS: 
+            return {
+                ...state,
+                fetching: false,
+                error: null,
+            }
         case SIGN_UP_FAILURE:
+            return {
+                ...state,
+                fetching: false,
+                error: action.payload
+            }
         
         default: 
             return state
