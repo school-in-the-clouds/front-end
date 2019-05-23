e593818166c7d14b2abc7c64965241291ceee50d
Moses Samuel <mosamuel98@gmail.com>
2019-05-22 17:15:45 -0700

    curry the "searchByCountry" action creator

--- src/actions/student.js
+++ src/actions/student.js
@@ -7,17 +7,19 @@
 
 // SEARCH (BY AVAILABLE TIMES OR COUNTRY)
 
+import { curry } from 'ramda'
+// See Line 6 of "./task.js" for quick primer on how to use curry
+
 export const SEARCH_INIT = "SEARCH_INIT"
 export const SEARCH_SUCCESS = "SEARCH_SUCCESS"
 export const SEARCH_FAILURE = "SEARCH_FAILURE"
 
-export const searchByCountry = (authToken, country) => (dispatch) => {
+export const searchByCountry = curry((authToken, country) => (dispatch) => {
     dispatch({ type: SEARCH_INIT })
     // this endpoint isn't set up yet, so let's intentionally deny this request
     setTimeout(() => {
         dispatch({ type: SEARCH_FAILURE })
-    }, 500)
-    
-}
+    }, 500)  
+})
 
 // STRETCH: SCHEDULE TIME TO WORK WITH VOLUNTEER
