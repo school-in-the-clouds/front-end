78f37ae1346ff255608d034d72c18f8c8d6c15ed
Moses Samuel <mosamuel98@gmail.com>
2019-05-22 19:01:17 -0700

    combine all reducers and export them

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,14 @@
-import authReducer from './auth'
 import { combineReducers }  from 'redux'
 
+import authReducer from './auth'
+import studentRedcuer from './student'
+import taskReducer from './task'
+import userReducer from './task';
+
+
 export default combineReducers({
-    auth: authReducer
-    //...
+    auth: authReducer,
+    student: studentRedcuer,
+    task: taskReducer,
+    user: userReducer
 })
